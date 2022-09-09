47aa13b37af9630f694ebf27f33ff970a7f0ac8b
losthakkun <losthakkun@gmail.com>
2022-09-09 00:06:30 -0500

    Initialize code

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+const options = {
+	arrowParens: 'avoid',
+	bracketSpacing: true,
+	endOfLine: 'lf',
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'none',
+	useTabs: true,
+	semi: true,
+};
+
+module.exports = options;
